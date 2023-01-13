099a8d40b7748f9ee34bc07783a3b4eb2dad5ed6
Amatuny <martinamatuny@gmail.com>
2023-01-13 16:18:51 +0400

    removed unnecessary comments

--- PasswordField/components/PasswordTextField.tsx
+++ PasswordField/components/PasswordTextField.tsx
@@ -33,38 +33,3 @@ export const PasswordTextField: React.FunctionComponent<IPasswordTextFieldProps>
     />
   );
 };
-
-// import * as React from 'react';
-// import { TextField } from '@fluentui/react';
-// import { fieldStyles } from '../styles/fieldStyles';
-
-// export interface IPasswordTextFieldProps {
-//   currentTextValue: string | null;
-//   onChange: (newTextValue : string | undefined) => void;
-//   isControlDisabled: boolean;
-// }
-
-// // eslint-disable-next-line max-len, react/display-name
-// export const PasswordTextField: React.FunctionComponent<IPasswordTextFieldProps> = React.memo(props => {
-//   console.log("a");
-//   // eslint-disable-next-line react/prop-types
-//   const { currentTextValue, onChange, isControlDisabled } = props;
-//   const [value, setValue] = React.useState(currentTextValue);
-//   React.useEffect(() => {
-//     setValue(currentTextValue);
-//   }, [currentTextValue]);
-//   return (
-//     <TextField
-//       type="password"
-//       canRevealPassword={!!currentTextValue}
-//       revealPasswordAriaLabel="Show password"
-//       styles={fieldStyles}
-//       value={value || ''}
-//       disabled={isControlDisabled}
-//       onChange={(_e, newValue) => {
-//         setValue(newValue || '');
-//         onChange(newValue);
-//       }}
-//     />
-//   );
-// });
