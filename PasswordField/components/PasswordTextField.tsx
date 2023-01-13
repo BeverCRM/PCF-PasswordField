c98c032b7cadb6098c65ea3748a6e02c42a51976
Amatuny <martinamatuny@gmail.com>
2023-01-13 16:14:26 +0400

    fixed space issue, optimized control

--- PasswordField/components/PasswordTextField.tsx
+++ PasswordField/components/PasswordTextField.tsx
@@ -10,18 +10,61 @@ export interface IPasswordTextFieldProps {
 
 export const PasswordTextField: React.FunctionComponent<IPasswordTextFieldProps> = props => {
   const { currentTextValue, onChange, isControlDisabled } = props;
+  const [value, setValue] = React.useState(currentTextValue);
+
+  React.useEffect(() => {
+    setValue(currentTextValue);
+  }, [currentTextValue]);
 
   return (
     <TextField
       type="password"
-      canRevealPassword={!!currentTextValue}
+      canRevealPassword
       revealPasswordAriaLabel="Show password"
       styles={fieldStyles}
-      value={currentTextValue || ''}
+      value={value || ''}
       disabled={isControlDisabled}
       onChange={(e, newValue) => {
-        onChange(newValue);
+        setValue(newValue || '');
+      }}
+      onBlur={e => {
+        onChange(e.target.value);
       }}
     />
   );
 };
+
+// import * as React from 'react';
+// import { TextField } from '@fluentui/react';
+// import { fieldStyles } from '../styles/fieldStyles';
+
+// export interface IPasswordTextFieldProps {
+//   currentTextValue: string | null;
+//   onChange: (newTextValue : string | undefined) => void;
+//   isControlDisabled: boolean;
+// }
+
+// // eslint-disable-next-line max-len, react/display-name
+// export const PasswordTextField: React.FunctionComponent<IPasswordTextFieldProps> = React.memo(props => {
+//   console.log("a");
+//   // eslint-disable-next-line react/prop-types
+//   const { currentTextValue, onChange, isControlDisabled } = props;
+//   const [value, setValue] = React.useState(currentTextValue);
+//   React.useEffect(() => {
+//     setValue(currentTextValue);
+//   }, [currentTextValue]);
+//   return (
+//     <TextField
+//       type="password"
+//       canRevealPassword={!!currentTextValue}
+//       revealPasswordAriaLabel="Show password"
+//       styles={fieldStyles}
+//       value={value || ''}
+//       disabled={isControlDisabled}
+//       onChange={(_e, newValue) => {
+//         setValue(newValue || '');
+//         onChange(newValue);
+//       }}
+//     />
+//   );
+// });
